b6d7947fa18fb54b35a208fb0a07113aa640aaa4
lukylun <seoyj505@naver.com>
2023-02-14 17:06:47 +0900

    20230214

--- codingtest_practice/code_execute.py
+++ codingtest_practice/code_execute.py
@@ -1,13 +1,46 @@
-string = input()
-stack = []
-answer = 0
-cnt = 0 
-for s in string:
-    if s == "(" or s == "[":
-        stack.append(s)
-    elif s == ")" or s == "]":
-        if len(stack) == 0:
-            break
-        if s == ")" and stack[-1] == "(":
-            answer += 2
-            stack.pop()
+# 방향 그래프일때
+def dfs(s, V, adj):
+    visited[s] = 1
+    if len[adj[s]] > 0:
+        for a in adj[s]:
+            if visited[a] != 1:
+                dfs(a, V, adj)
+    else:
+        return 99
+
+# 무방향 그래프
+def dfs_non(s, V):
+    visited = [0] * (V + 1)
+    stack = []
+    i = s
+    visited[i] = 1
+    while True:
+        for w in adj_list[i]:
+            if visited[w] == 0:
+                stack.append(w)
+                i = w
+                visited[w] = 1
+                print(i)
+                # ans.append(i)
+                break
+        else:
+            if stack:
+                i = stack.pop()
+            else:
+                break
+    return visited
+
+V = int(input())
+n = int(input())
+visited = [0] * (V + 1)
+adj_list = [[] for _ in range(V + 1)]
+for i in range(n):
+    start, to = map(int, input().split())
+    adj_list[start].append(to)
+    adj_list[to].append(start)
+print(dfs_non(1, V))
+
+# print(9ans)
+    
+
+
