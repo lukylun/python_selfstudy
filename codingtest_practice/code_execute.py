b515887f0a857068fb04b236f4a1aa92e3ed71a3
scott <seoyj505@naver.com>
2023-02-19 18:58:13 +0900

    20230219

--- codingtest_practice/code_execute.py
+++ codingtest_practice/code_execute.py
@@ -1,12 +1,17 @@
-N = int(input()) # 빌딩 수
-buildings = [int(input().rstrip()) for _ in range(N)]
-stack = []
-ans = 0
+l_stack = list(input())
+r_stack = []
+N = int(input())
 
-for building in buildings:
-    while stack and stack[-1] < building:
-        stack.pop()
-    stack.append(building)
-    ans += len(stack) - 1
-    
-print(ans)
+for i in range(N):
+    string = input()
+    if string[0] == 'P':
+        string = string.split()
+        l_stack.append(string[-1])
+    elif string == 'L' and l_stack:
+        r_stack.append(l_stack.pop())
+    elif string == 'D' and r_stack:
+        l_stack.append(r_stack.pop())
+    elif string == 'B' and l_stack:
+        l_stack.pop()
+
+print(''.join(l_stack)+''.join(r_stack[::-1]))
