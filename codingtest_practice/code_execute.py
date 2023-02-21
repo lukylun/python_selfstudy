43319373ddb1ea9abe382221d10421854b32ee5a
scotty <seoyj505@naver.com>
2023-02-21 17:57:06 +0900

    20230221

--- codingtest_practice/code_execute.py
+++ codingtest_practice/code_execute.py
@@ -1,23 +1,21 @@
-for _ in range(10):
-    tc = int(input())
-    passwords = list(map(int, input().split()))
-    nums = [x for x in range(1, 6)]
+import sys
+from collections import deque
 
-    front = 0
-    i = 0
-    while passwords[front] > 0:        
-        passwords[front] -= nums[i]
-        
-        if passwords[front] <= 0:
-            passwords[front] = 0
-            break
-        
-        # front, i 값을 1씩 증가
-        front = (front + 1) % 8
-        i = (i + 1) % 5
-    
-    # passwords의 0값의 index
-    idx = passwords.index(0)
-    print(f'{tc} {" ".join(map(str, passwords[idx + 1:]))} {" ".join(map(str,(passwords[:idx + 1])))}')
+T = int(input())
+q = deque()
 
-    
+for tc in range(T):
+    N, M = map(int, input().split())
+    lst = list(map(int, input().split()))
+    cnt = 0
+    while True:
+        for i in range(N):
+            if lst[i] < max(lst):
+                q.append(i)
+            else:
+                chr = q.popleft()
+                if chr == M:
+                    print(cnt)
+                    break
+                else:                    
+                    cnt += 1
