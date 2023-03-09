0a187c04089d88d0790d6f4ba708e60cfee35126
scott <seoyj505@naver.com>
2023-03-09 23:47:46 +0900

    20230309

--- codingtest_practice/code_execute.py
+++ codingtest_practice/code_execute.py
@@ -1,27 +1,33 @@
 from collections import deque
-N, M = map(int, input().split())
-maps = []
-shark = deque()
+
+N, M, K = map(int, input().split())
+maps = [[0] * M for _ in range(N)]
 v = [[0] * M for _ in range(N)]
+q = deque()
+
+for _ in range(K):
+    a, b, x, y = map(int, input().split())
+    for r in range(b, y):
+        for c in range(a, x):
+            maps[r][c] = 1
+
+ans = []
 for i in range(N):
-    lst = list(map(int, input().split()))
-    maps.append(lst)
     for j in range(M):
-        if lst[j] == 1:
-            shark.append((i, j))
-
-
-while shark:
-    r, c = shark.popleft()
-    for d in ((0, 1), (0, -1), (1, 0), (1, 1), (1, -1), (-1, -1), (-1, 1), (-1, 0)):
-        nr = r + d[0]
-        nc = c + d[1]
-        if 0 <= nr < N and 0 <= nc < M and not maps[nr][nc]:
-            maps[nr][nc] = maps[r][c] + 1
-            shark.append((nr, nc))
-
-ans = 0
-for i in range(N):
-    if max(maps[i]) > ans:
-        ans = max(maps[i])
-print(ans - 1)
+        cnt = 1
+        if maps[i][j] == 0:
+            q.append((i, j))
+            while q:
+                r, c = q.popleft()
+                maps[r][c] = 1
+                for dr, dc in ((0, 1), (0, -1), (1, 0), (-1, 0)):
+                    nr = r + dr
+                    nc = c + dc
+                    if 0 <= nr < N and 0 <= nc < M and not v[nr][nc] and not maps[nr][nc]:
+                        q.append((nr, nc))
+                        v[nr][nc] = 1
+                        cnt += 1
+            ans.append(cnt)
+ans.sort()
+print(len(ans))
+print(' '.join(map(str, ans)))
