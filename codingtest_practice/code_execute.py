ed5907a73d7750b8fb38d256ccbc2ea3aeb0f70e
scotty <seoyj505@naver.com>
2023-03-09 17:55:40 +0900

    20230309

--- codingtest_practice/code_execute.py
+++ codingtest_practice/code_execute.py
@@ -1,10 +1,27 @@
-N, M, B = map(int, input().split())
+from collections import deque
+N, M = map(int, input().split())
 maps = []
-nums = set()
-for _ in range(N):
+shark = deque()
+v = [[0] * M for _ in range(N)]
+for i in range(N):
     lst = list(map(int, input().split()))
     maps.append(lst)
-    for i in range(M):
-        nums.add(lst[i])
+    for j in range(M):
+        if lst[j] == 1:
+            shark.append((i, j))
 
-print(arr)
+
+while shark:
+    r, c = shark.popleft()
+    for d in ((0, 1), (0, -1), (1, 0), (1, 1), (1, -1), (-1, -1), (-1, 1), (-1, 0)):
+        nr = r + d[0]
+        nc = c + d[1]
+        if 0 <= nr < N and 0 <= nc < M and not maps[nr][nc]:
+            maps[nr][nc] = maps[r][c] + 1
+            shark.append((nr, nc))
+
+ans = 0
+for i in range(N):
+    if max(maps[i]) > ans:
+        ans = max(maps[i])
+print(ans - 1)
