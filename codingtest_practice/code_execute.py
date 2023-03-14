dc8d6beb108a5488d3b615bce0b7cd8460412e98
scott <seoyj505@naver.com>
2023-03-15 01:12:33 +0900

    20230315

--- codingtest_practice/code_execute.py
+++ codingtest_practice/code_execute.py
@@ -1,26 +1,13 @@
-N = int(input())
+a = list(input())
+b = list(input())
+d = [0] * 1001
 
-d = [[0] * 10 for _ in range(N+1)]
+for i in range(len(a)):
+    cnt = 0
+    for j in range(len(b)):
+        if cnt < d[j]:
+            cnt = d[j]
+        elif a[i] == b[j]:
+            d[j] = cnt + 1
 
-for i in range(1, 10):
-    d[1][i] = 1
-
-for i in range(2, N+1):
-    for j in range(10):
-        if j == 0:
-            d[i][j] = d[i-1][1]
-        elif 1 <= j <= 8:
-            d[i][j] = d[i-1][j-1] + d[i-1][j+1]
-        else:
-            d[i][j] = d[i-1][8]
-
-print(sum(d[N]) % 1000000000)
-
-N = int(input())
-d = [0] * N
-d[0] = 9
-
-for i in range(1, N):
-    d[i] = d[i-1] * 2 - i
-
-print(d[N-1])
+print(max(d))
