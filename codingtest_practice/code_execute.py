5af8df64d4fc0264e6c016e1e716a27926c1ba55
scott <seoyj505@naver.com>
2023-03-17 00:41:31 +0900

    20230317

--- codingtest_practice/code_execute.py
+++ codingtest_practice/code_execute.py
@@ -1,34 +1,9 @@
-# N = int(input())
-# lst = [list(map(int, input().split())) for _ in range(N)]
-# dp = [[0] * (N+1) for _ in range(N)]
-# dp[0][0] = lst[0][0]
-# for i in range(1, N):
-#     for j in range(len(lst[i])):
-#         if j == 0:
-#             dp[i][j] = dp[i-1][0] + lst[i][j]
-#         elif j == len(lst[i]) - 1:
-#             dp[i][j] = dp[i-1][j-1] + lst[i][j]
-#         else:
-#             dp[i][j] = max(dp[i-1][j-1], dp[i-1][j]) + lst[i][j]
-#
-# print(dp)
+N, M = map(int, input().split())
 
+ans = 1
+cnt = 1
+for i in range(N, N - M, -1):
+    ans *= i
+    ans //= (N + 1 - i)
 
-def dfs(x, lst, alpha):
-    lst.append(x)
-    for i in alpha:
-        if len(x) != len(alpha):
-            dfs(x + i, lst, alpha)
-        else:
-            x[:-1]
-
-
-def solution(word):
-    vowels = ["A", "E", "I", "O", "U"]
-    ans = []
-    for i in range(len(vowels)):
-        dfs(vowels[i], ans, vowels)
-
-    return ans.index(word) + 1
-
-print(solution("AAAAE"))
+print(ans)
