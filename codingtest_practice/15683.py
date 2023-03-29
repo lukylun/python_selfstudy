7b863d33d46bd182ec005033b3e4cc9566c54c99
scott <seoyj505@naver.com>
2023-03-29 17:58:26 +0900

    20230329

--- codingtest_practice/15683.py
+++ codingtest_practice/15683.py
@@ -25,3 +25,16 @@ for r, c in cctv:
 # print(v)
 # print(cctv)
 
+# 퀵 정렬
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[0]
+    tail = arr[1:]
+
+    left_side = [x for x in tail if x <= pivot]
+    right_side = [x for x in tail if x > pivot]
+
+    return quick_sort(left_side) + [pivot] + quick_sort(right_side)
+
+quick_sort(num_list)
