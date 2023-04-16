3a7b4d9a4c0cbd498be970eec3c7005f3982f58d
scott <seoyj505@naver.com>
2023-04-16 23:48:49 +0900

    20230416

--- codingtest_practice/Baekjoon/sample.py
+++ codingtest_practice/Baekjoon/sample.py
@@ -1,2 +1,10 @@
-a= [1,1,1]
-print(a.count)
+N = int(input())
+coins = [500, 100, 50, 10, 5, 1]
+
+changes = 1000 - N
+ans = 0
+for coin in coins:
+    ans += (changes // coin)
+    changes %= coin
+
+print(ans)
