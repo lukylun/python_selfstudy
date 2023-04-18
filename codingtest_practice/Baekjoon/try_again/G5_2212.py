2e5beac8e8a7ae6467658ef0a6452b3260d63ebf
scott <seoyj505@naver.com>
2023-04-18 21:17:14 +0900

    2023

--- codingtest_practice/Baekjoon/try_again/G5_2212.py
+++ codingtest_practice/Baekjoon/try_again/G5_2212.py
@@ -6,26 +6,31 @@ temp = []
 for i in range(N-1):
     temp.append(lst[i+1]-lst[i])
 
-maxV = max(temp)
-
-result = []
-minV = 0
-
-cnt = 0
-for i in range(N-1):
-    if K > 1:
-        if cnt + lst[i+1] - lst[i] >= maxV:
-            minV += cnt
-            cnt = 0
-            K -= 1
-        elif cnt + lst[i+1] - lst[i] < maxV:
-            cnt += lst[i+1] - lst[i]
-
-    else:
-        cnt += lst[i+1] - lst[i]
-
-    if i == N - 2:
-        minV += cnt
-
-
-print(minV)
+temp.sort()
+if K >= N:
+    print(0)
+else:
+    print(sum(temp[:N-K]))
+# maxV = max(temp)
+#
+# result = []
+# minV = 0
+#
+# cnt = 0
+# for i in range(N-1):
+#     if K > 1:
+#         if cnt + lst[i+1] - lst[i] >= maxV:
+#             minV += cnt
+#             cnt = 0
+#             K -= 1
+#         elif cnt + lst[i+1] - lst[i] < maxV:
+#             cnt += lst[i+1] - lst[i]
+#
+#     else:
+#         cnt += lst[i+1] - lst[i]
+#
+#     if i == N - 2:
+#         minV += cnt
+#
+#
+# print(minV)
