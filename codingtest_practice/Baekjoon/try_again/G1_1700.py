57e2e2fc69b07e3d0c5214a06fa3e5feaf9e533c
lukylun <seoyj505@naver.com>
2023-04-18 17:54:22 +0900

    2023

--- codingtest_practice/Baekjoon/try_again/G1_1700.py
+++ codingtest_practice/Baekjoon/try_again/G1_1700.py
@@ -1,2 +1,31 @@
+'''
+3 5
+1 1 1 1 2
+3 9
+1 2 3 4 1 1 1 1 3
+'''
+
 N, K = map(int, input().split())
 lst = list(map(int, input().split()))
+
+ans = set()
+num_dict = {}
+
+for i in lst:
+    if i in num_dict:
+        num_dict[i] += 1
+    else:
+        num_dict[i] = 1
+
+cnt = 0
+for i in lst:
+    ans.add(i)
+    num_dict[i] -= 1
+    if len(ans) > N:
+        for j in lst:
+            if num_dict[i] == 0 and i in ans:
+                ans.remove(i)
+                break
+        else:
+            ans.remove
+        cnt += 1
---
+++ codingtest_practice/Baekjoon/try_again/G5_2212.py
@@ -0,0 +1,31 @@
+N = int(input())
+K = int(input())
+lst = sorted(list(map(int, input().split())))
+
+temp = []
+for i in range(N-1):
+    temp.append(lst[i+1]-lst[i])
+
+maxV = max(temp)
+
+result = []
+minV = 0
+
+cnt = 0
+for i in range(N-1):
+    if K > 1:
+        if cnt + lst[i+1] - lst[i] >= maxV:
+            minV += cnt
+            cnt = 0
+            K -= 1
+        elif cnt + lst[i+1] - lst[i] < maxV:
+            cnt += lst[i+1] - lst[i]
+
+    else:
+        cnt += lst[i+1] - lst[i]
+
+    if i == N - 2:
+        minV += cnt
+
+
+print(minV)
